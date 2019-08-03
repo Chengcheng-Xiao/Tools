03a2f4d4b6dc44fb9ffc85a21098f4d754c11293
Chengcheng Xiao <iconxicon@me.com>
2019-08-03 14:15:30 +0800

    add spincar script.

---
+++ VASP/spincar.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python
+"""
+A script to extract spindensity form CHGCAR file.
+User must specify filename in command line.
+eg. python spincar.py CHGCAR
+** Does NOT work with Molecular dynamics output.
+Depends on ase
+"""
+
+import os
+import sys
+import numpy as np
+import math
+import string
+import datetime
+import time
+from ase.calculators.vasp import VaspChargeDensity
+
+
+starttime = time.clock()
+print "Starting calculation at",
+print time.strftime("%H:%M:%S on %a %d %b %Y")
+
+# Find out how many arguments were on the command line,
+nsubtract = len(sys.argv)-1
+if not nsubtract == 1:
+    print "\n** ERROR: Must specify the name of file on command line."
+    print "eg. chgdiff.py CHGCAR"
+    print "Only one file name are allowed"
+    sys.exit(0)
+
+if not os.path.isfile(sys.argv[1]):
+    print "\n** ERROR: Input file %s was not found." % sys.argv[1]
+    sys.exit(0)
+
+
+# Read information from command line
+# First specify location of CHGCAR
+CHGCARfile = sys.argv[1].lstrip()
+
+
+# Open geometry and density class objects
+#-----------------------------------------
+vasp_charge = VaspChargeDensity(filename = CHGCARfile)
+if len(vasp_charge.chgdiff) == 3:
+    spin=3
+    print "\nReading spin orbital potential data from file %s ... \n" % CHGCARfile,
+    atoms = vasp_charge.atoms[-1]
+    potl_total = vasp_charge.chg[-1]
+    potl_x = vasp_charge.chgdiff[0]
+    potl_y = vasp_charge.chgdiff[1]
+    potl_z = vasp_charge.chgdiff[2]
+    if not potl_total.shape == potl_x.shape == potl_y.shape == potl_z.shape:
+        print "\n**ERROR: Two sets of data are not on the same grid."
+        print "Data from data block 1 on %dx%dx%d grid." % (potl_total.shape[0],potl_total.shape[1],potl_total.shape[2])
+        print "Data from data block 2 on %dx%dx%d grid." % (potl_x.shape[0],potl_x.shape[1],potl_x.shape[2])
+        print "Data from data block 3 on %dx%dx%d grid." % (potl_y.shape[0],potl_y.shape[1],potl_y.shape[2])
+        print "Data from data block 4 on %dx%dx%d grid." % (potl_z.shape[0],potl_z.shape[1],potl_z.shape[2])
+        sys.exit(0)
+
+elif len(vasp_charge.chgdiff) == 1:
+    spin=2
+    print "\nReading spin polarized potential data from file %s ... \n" % CHGCARfile,
+    atoms = vasp_charge.atoms[-1]
+    potl_updown = vasp_charge.chg[-1]
+    potl_upmdown = vasp_charge.chgdiff[0]
+    if not potl_updown.shape == potl_upmdown.shape:
+        print "\n**ERROR: Two sets of data are not on the same grid."
+        print "Data from data block 1 on %dx%dx%d grid." % (potl_updown.shape[0],potl_updown.shape[1],potl_updown.shape[2])
+        print "Data from data block 2 on %dx%dx%d grid." % (potl_updown.shape[0],potl_upmdown.shape[1],potl_upmdown.shape[2])
+        sys.exit(0)
+    else:
+        # get density for each spin channels
+        potl_up = (potl_updown+potl_upmdown)/2
+        potl_down = (potl_updown-potl_upmdown)/2
+
+elif len(vasp_charge.chgidff) == 0:
+    spin=1
+    print "\nReading spin paired potential data from file %s ... \n" % CHGCARfile,
+    print "File only contains one block of data, check if is polarized calculation."
+    sys.exit(0)
+
+else:
+    print "\n** ERROR: CHGCAR data block error, total of %s data blocks was found." % len(vasp_charge.chg)
+    sys.exit(0)
+
+del vasp_charge
+
+
+# Exctract data
+#------------------------
+if spin == 3: # SOC
+    chg_total = VaspChargeDensity(filename=None)
+    chg_total.atoms=[atoms,]
+    chg_total.chg=[potl_total,]
+
+    chg_x = VaspChargeDensity(filename=None)
+    chg_x.atoms=[atoms,]
+    chg_x.chg=[potl_x,]
+
+    chg_y = VaspChargeDensity(filename=None)
+    chg_y.atoms=[atoms,]
+    chg_y.chg=[potl_y,]
+
+    chg_z = VaspChargeDensity(filename=None)
+    chg_z.atoms=[atoms,]
+    chg_z.chg=[potl_z,]
+
+    # Print out charge density
+    #--------------------------
+    # Check whether CHGDIFF exists
+    for chgfiles in ['SPINCAR_total.vasp','SPINCAR_x.vasp','SPINCAR_y.vasp','SPINCAR_z.vasp']:
+        if os.path.isfile("./"+i):
+            print "\n**WARNING: A file called CHGDIFF already exists in this directory."
+            yesno=raw_input("Type y to continue and overwrite it, any other key to stop\n")
+            if yesno!="y":
+                sys.exit(0)
+    # Writing data ...
+    print "Writing density difference data to file ...",
+    sys.stdout.flush()
+    chg_total.write(filename="SPINCAR_total.vasp",format="chgcar")
+    chg_x.write(filename="SPINCAR_x.vasp",format="chgcar")
+    chg_y.write(filename="SPINCAR_y.vasp",format="chgcar")
+    chg_z.write(filename="SPINCAR_z.vasp",format="chgcar")
+    print "done."
+
+if spin == 2: # Spin polarized
+    chg_updown = VaspChargeDensity(filename=None)
+    chg_updown.atoms=[atoms,]
+    chg_updown.chg=[potl_updown,]
+
+    chg_upmdown = VaspChargeDensity(filename=None)
+    chg_upmdown.atoms=[atoms,]
+    chg_upmdown.chg=[potl_upmdown,]
+
+    chg_up = VaspChargeDensity(filename=None)
+    chg_up.atoms=[atoms,]
+    chg_up.chg=[potl_up,]
+
+    chg_down = VaspChargeDensity(filename=None)
+    chg_down.atoms=[atoms,]
+    chg_down.chg=[potl_down,]
+
+    # Print out charge density
+    #--------------------------
+    # Check whether CHGDIFF exists
+    for chgfiles in ['SPINCAR_total.vasp','SPINCAR_up+own.vasp','SPINCAR_up-down.vasp','SPINCAR_up.vasp','SPINCAR_down.vasp']:
+        if os.path.isfile("./"+chgfiles):
+            print "\n**WARNING: A file called ' " + chgfiles + " ' already exists in this directory."
+            yesno=raw_input("Type y to continue and overwrite it, any other key to stop\n")
+            if yesno!="y":
+                sys.exit(0)
+    # Writing data ...
+    print "\nWriting density difference data to file ...",
+    sys.stdout.flush()
+    chg_updown.write(filename="SPINCAR_up+down.vasp",format="chgcar")
+    chg_upmdown.write(filename="SPINCAR_up-down.vasp",format="chgcar")
+    chg_up.write(filename="SPINCAR_up.vasp",format="chgcar")
+    chg_down.write(filename="SPINCAR_down.vasp",format="chgcar")
+    print "done."
+
+# Post process
+#-------------------
+endtime = time.clock()
+runtime = endtime-starttime
+print "\nEnd of calculation."
+print "Program was running for %.2f seconds." % runtime
