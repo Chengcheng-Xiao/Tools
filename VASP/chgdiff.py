56f87cba12de39b9ea9d8996174592fe39352776
Chengcheng Xiao <iconxicon@me.com>
2019-12-09 10:28:02 +0000

    enhance chgdiff.py

--- VASP/chgdiff.py
+++ VASP/chgdiff.py
@@ -14,7 +14,7 @@ from ase.calculators.vasp import VaspChargeDensity
 
 starttime = time.clock()
 print "Starting calculation at",
-print time.strftime("%H:%M:%S on %a %d %b %Y")
+print time.strftime("%H:%M:%S on %a %d %b %Y \n")
 
 # Find out how many arguments were on the command line,
 # all but the first two should contain files with densities
@@ -65,13 +65,14 @@ for CHGCARfile2 in sys.argv[2:]:
        print "Data from file %s on %dx%dx%d grid.\n" % (CHGCARfile2,chg2.shape[0],chg2.shape[1],chg2.shape[2])
        sys.exit(0)
     else:
-       print "Subtracting data from file %s ..." % CHGCARfile2,
+       print "Subtracting %s from file %s ..." % (CHGCARfile2, CHGCARfile1),
        sys.stdout.flush()
 
     # Take difference
     #-----------------
     chgdiff=chgdiff-chg2
     print "done."
+
 
 vasp_charge_diff = VaspChargeDensity(filename=None)
 vasp_charge_diff.atoms=[atoms1,]
@@ -80,7 +81,7 @@ vasp_charge_diff.chg=[chgdiff,]
 # Print out charge density
 #--------------------------
 # Check whether CHGDIFF exists
-if os.path.isfile("./CHGDIFF"):
+if os.path.isfile("./CHGDIFF.vasp"):
    print "\n**WARNING: A file called CHGDIFF already exists in this directory."
    yesno=raw_input("Type y to continue and overwrite it, any other key to stop\n")
    if yesno!="y":
@@ -90,6 +91,26 @@ if os.path.isfile("./CHGDIFF"):
 print "Writing density difference data to file CHGDIFF ...",
 sys.stdout.flush()
 vasp_charge_diff.write(filename="CHGDIFF.vasp",format="chgcar")
+
+# adding atomic species
+fin = [atoms1.get_chemical_symbols()[0]]
+for i in range(len(atoms1.get_chemical_symbols())):
+    if i == len(atoms1.get_chemical_symbols())-1: break
+    if atoms1.get_chemical_symbols()[i] != atoms1.get_chemical_symbols()[i+1]:
+        fin.append(atoms1.get_chemical_symbols()[i+1])
+
+for chgfiles in ['CHGDIFF.vasp']:
+    f = open(chgfiles, "r")
+    contents = f.readlines()
+    f.close()
+
+    contents.insert(5, ' '.join(fin)+'\n')
+
+    f = open(chgfiles, "w")
+    contents = "".join(contents)
+    f.write(contents)
+    f.close()
+
 print "done."
 
 endtime = time.clock()
