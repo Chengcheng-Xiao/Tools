3e0d52dedb077e621e40797a509e3dfe12df9a68
Chengcheng Xiao <iconxicon@me.com>
2020-03-12 15:05:52 +0000

    add dipole moment output in chgcent.py

--- VASP/chgcent.py
+++ VASP/chgcent.py
@@ -6,7 +6,7 @@ eg. python chgcent.py CHGCAR
 Depends on ase
 """
 
-
+import argparse
 import os
 import sys
 import numpy as np
@@ -16,6 +16,7 @@ import datetime
 import time
 from ase.calculators.vasp import VaspChargeDensity
 
+np.set_printoptions(formatter={'float': '{: 0.5f}'.format})
 
 starttime = time.clock()
 print "Starting calculation at",
@@ -39,14 +40,27 @@ potl = vasp_charge.chg[-1]
 atoms = vasp_charge.atoms[-1]
 del vasp_charge
 
+# Open outcar to find zval and ions_per_type
+#-----------------------------------------
+with open("OUTCAR") as search:
+    for line in search:
+        line = line.rstrip()  # remove '\n' at end of line
+        if "ZVAL" in line:
+            ZVAL = line.split()[2:]
+        elif "ions per type" in line:
+            ions_per_type = line.split()[4:]
 
-print "\nReading file: %s" % LOCPOTfile
+
+print "\nElectronic part"
+print "-----------------"
+print "Reading file: %s" % LOCPOTfile
 #print "Performing average in %s direction" % direction
 
 
-# Read in lattice parameters and scale factor
+# Read in lattice parameters and scale factor and atomic positions
 #---------------------------------------------
 cell = atoms.cell
+pos = atoms.get_positions()
 
 
 # Find length of lattice vectors
@@ -126,17 +140,59 @@ for i in range(ngridpts[2]):
     z = float(i)/ngridpts[2]
     avg_z += z*average_z[i]/average_z.sum()
 
+# Total electron numbers
+#-------------------
+total_elect = 0
+for i in range(len(ZVAL)):
+    total_elect += int(ions_per_type[i])*int(float(ZVAL[i]))
 
 # Print out average
 #-------------------
 print "\nCell matrix:"
 print cell
 
-print "\nCrystal axis length (a,b,c) = %15.8g , %15.8g , %15.8g" % (latticelength[0],latticelength[1], latticelength[2])
+print "\nCrystal axis length (a,b,c)      = %8.5f , %8.5f , %8.5f" % (latticelength[0],latticelength[1], latticelength[2])
+print "Charge center (crystal axis)     = %8.5f , %8.5f , %8.5f" % (avg_x, avg_y, avg_z)
+print "Charge center (Cartisen axis)    = %8.5f , %8.5f , %8.5f" % (avg_x*cell[0,0]+avg_y*cell[1,0]+avg_z*cell[2,0], avg_x*cell[0,1]+avg_y*cell[1,1]+avg_z*cell[2,1], avg_x*cell[0,2]+avg_y*cell[1,2]+avg_z*cell[2,2])
 
-print "\nCharge center (crystal axis) = %15.8g , %15.8g , %15.8g" % (avg_x, avg_y, avg_z)
+sys.stdout.write("\033[0;32m") # set color green
+print "\nTotal elect dipole moments (e*A) =  %8.5f  %8.5f  %8.5f" % (total_elect*(avg_x*cell[0,0]+avg_y*cell[1,0]+avg_z*cell[2,0]), total_elect*(avg_x*cell[0,1]+avg_y*cell[1,1]+avg_z*cell[2,1]), total_elect*(avg_x*cell[0,2]+avg_y*cell[1,2]+avg_z*cell[2,2]))
+sys.stdout.write("\033[0;0m") # reset color
 
-print "\nCharge center (Cartisen axis) = %15.8g , %15.8g , %15.8g" % (avg_x*cell[0,0] + avg_y*cell[1,0] + avg_z*cell[2,0], avg_x*cell[0,1] + avg_y*cell[1,1] + avg_z*cell[2,1], avg_x*cell[0,2] + avg_y*cell[1,2] + avg_z*cell[2,2])
+# get ion types and numbers and val
+#-------------------
+print "\nIonic part"
+print "-----------------"
+
+print "ZVAL          = ", ZVAL
+
+print "ions per type = ", ions_per_type
+
+
+if len(ions_per_type) != len(ZVAL):
+    print("len(ions_per_type) != len(ZVAL)")
+    sys.exit(0)
+
+print "\nionic positions (Cartisen) for each type:"
+for i in range(len(ZVAL)):
+    print "type : %i" % int(i)
+    print pos[sum(int(n) for n in (ions_per_type[:i])):sum(int(n) for n in (ions_per_type[:i])) + int(ions_per_type[i])]
+
+# calculate total ionic dipole moments
+total_ion_dipole = [0,0,0]
+for i in range(len(ZVAL)):
+    #print "type : %i" % int(i)
+    #print int(float(ZVAL[i]))*pos[sum(int(n) for n in (ions_per_type[:i])):sum(int(n) for n in (ions_per_type[:i])) + int(ions_per_type[i])]
+    total_ion_dipole += sum(int(float(ZVAL[i]))*pos[sum(int(n) for n in (ions_per_type[:i])):sum(int(n) for n in (ions_per_type[:i])) + int(ions_per_type[i])])
+
+sys.stdout.write("\033[0;32m") # set color green
+print "\nTotal ionic dipole moments (e*A) =  %8.5f  %8.5f  %8.5f" % (total_ion_dipole[0], total_ion_dipole[1], total_ion_dipole[2])
+
+sys.stdout.write("\033[1;31m" ) # set color red
+print "\nTotal dipole moments (e*A)       =  %8.5f  %8.5f  %8.5f" % (total_ion_dipole[0]-total_elect*(avg_x*cell[0,0]+avg_y*cell[1,0]+avg_z*cell[2,0]), total_ion_dipole[1]-total_elect*(avg_x*cell[0,1]+avg_y*cell[1,1]+avg_z*cell[2,1]), total_ion_dipole[2]-total_elect*(avg_x*cell[0,2]+avg_y*cell[1,2]+avg_z*cell[2,2]))
+sys.stdout.write("\033[0;0m") # reset color
+
+
 
 # Post process
 #-------------------
@@ -144,4 +200,3 @@ endtime = time.clock()
 runtime = endtime-starttime
 print "\nEnd of calculation."
 print "Program was running for %.2f seconds." % runtime
-
